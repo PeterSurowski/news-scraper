ac7deeaa7987e940da9a395d8afe0ab95339e2c0
PeterSurowski <petersurowski@yahoo.com>
2017-10-20 21:15:57 -0700

    Adding to /scrape path, getting an error when hitting /scrape path.

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var cheerio = require("cheerio");
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models:
-//var db = require("./models");
+var db = require("./models");
 var PORT = 3000;
 
 // Initialize Express
@@ -45,7 +45,50 @@ mongoose.connect("mongodb://localhost/week18Populater", {
   useMongoClient: true
 });
 
+app.get("/scrape", function(req, res) {
+  console.log('Get path is working!');
+  axios.get("http://www.echojs.com/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article
+        .create(result)
+        .then(function(dbArticle) {
+          // If we were able to successfully scrape and save an Article, send a message to the client
+          res.send("Scrape Complete");
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          res.json(err);
+        });
+    });
+  });
+});  
+  /* STOLEN FROM HANDLEBARS WISH SERVER.JS
+    connection.query("SELECT * FROM wishes;", function(err, data) {
+      if (err) {
+        throw err;
+      }
+      res.render("index", { wishes: data });
+    });
+  });
+  */
+
 // Start the server:
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
-});
+})
